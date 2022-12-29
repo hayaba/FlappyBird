8b16f76a854f2bed1477eb5d8a70ef47749676fd
Haya Barakat <56253728+hayaba@users.noreply.github.com>
2022-12-29 23:24:43 +0100

    Update Move.cs

--- Move.cs
+++ Move.cs
@@ -7,12 +7,6 @@ public class Move : MonoBehaviour
 
     public float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
